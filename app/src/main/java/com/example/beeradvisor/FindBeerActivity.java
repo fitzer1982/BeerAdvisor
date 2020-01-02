50f40ca9b928695da6f3054a76aedb1cd0cab007
fitzer1982 <pbfitzsimons@gmail.com>
2020-01-02 21:23:14 +0000

    Initial commit

---
+++ app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
@@ -0,0 +1,16 @@
+package com.example.beeradvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import static com.example.beeradvisor.R.layout.activity_find_beer;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_find_beer);
+    }
+}
