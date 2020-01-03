837808ed8886d3cfee297033bb7ed5247920fd6c
fitzer1982 <pbfitzsimons@gmail.com>
2020-01-03 15:41:45 +0000

    update

--- app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
+++ app/src/main/java/com/example/beeradvisor/FindBeerActivity.java
@@ -3,14 +3,38 @@ package com.example.beeradvisor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
 
 import static com.example.beeradvisor.R.layout.activity_find_beer;
 
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_find_beer);
+    }
+
+    public void onClickFindBeer(View view){
+        // get a reference to the textview
+        TextView brands = (TextView) findViewById(R.id.brands);
+        // get a refernce to the spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //get the selected item in the spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //get the list of beers
+        List<String> brandsList =expert.getBrands(beerType);
+        //build a string using the values in the list
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        // display the beers
+        brands.setText(brandsFormatted);
+
     }
 }
